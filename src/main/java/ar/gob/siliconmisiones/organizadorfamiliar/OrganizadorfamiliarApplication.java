6a11bd65827c2754849bb7734e910bae8503051f
Damian <damian.arganaras@gmail.com>
2021-03-18 23:49:42 -0300

    3rd
    Subidos los templates de Lucas, falta adaptarlos y retocarlos

--- src/main/java/ar/gob/siliconmisiones/organizadorfamiliar/OrganizadorfamiliarApplication.java
+++ src/main/java/ar/gob/siliconmisiones/organizadorfamiliar/OrganizadorfamiliarApplication.java
@@ -2,6 +2,10 @@ package ar.gob.siliconmisiones.organizadorfamiliar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class OrganizadorfamiliarApplication {
--- src/main/java/ar/gob/siliconmisiones/organizadorfamiliar/rest/UsuarioRestController.java
+++ src/main/java/ar/gob/siliconmisiones/organizadorfamiliar/rest/UsuarioRestController.java
@@ -4,8 +4,11 @@ package ar.gob.siliconmisiones.organizadorfamiliar.rest;
 import ar.gob.siliconmisiones.organizadorfamiliar.dao.UsuarioDAO;
 import ar.gob.siliconmisiones.organizadorfamiliar.entity.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,6 +29,17 @@ public class UsuarioRestController {
     @GetMapping("/Usuario")
     public List<Usuario> findAll() {
         return usuarioDAO.findAll();
+    }
+
+    @Controller
+    public class GreetingController {
+
+        @GetMapping("/greeting")
+        public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+            model.addAttribute("name", name);
+            return "./mi_perfil/miPerfil";
+        }
+
     }
 
 }
